f9be57bd5952031f92bf888bc12df0734cb3a6e7
alfrenardi <alfrenardi@gmail.com>
2012-03-27 00:03:24 +0300

    Update FastADC.h

--- FastADC.h
+++ FastADC.h
@@ -4,14 +4,15 @@ class FastADC {
     	FastADC();
     	void StartADCRes(int);
         void StartADCSpeed(int);
+        void StartADCMaxSpeed(int);
         void Stop();
         int Get();
         
     private:
-		char Old_ADCSRB;
-		char Old_ADCSRA;
-		char Old_ADMUX;
-		char OnFastADC;
+	char Old_ADCSRB;
+	char Old_ADCSRA;
+	char Old_ADMUX;
+	char OnFastADC;
 };
 
 extern FastADC FreeRunningADC;
