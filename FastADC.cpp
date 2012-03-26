ed6dafbc029d239f0a4755b40472dfd506f55437
alfrenardi <alfrenardi@gmail.com>
2012-03-26 23:59:19 +0300

    Update FastADC.cpp

--- FastADC.cpp
+++ FastADC.cpp
@@ -6,16 +6,16 @@
 //======BUFFERS
 
 static volatile int analogValue;
-static volatile int newData;
+//static volatile int newData;
 
 //======INTERRUPT VECTOR
 
 ISR(ADC_vect)
 {
-	volatile int sreg = SREG;	//saving the status register
+//	volatile int sreg = SREG;	//saving the status register
 	analogValue=ADC;			//putting the analog value in the buffer
-	newData = 1;				//new data available
-	SREG = sreg;				//restoring the status register
+	OnFastADC = 2;				//new data available
+//	SREG = sreg;				//restoring the status register
 }
 
 //=======CONSTRUCTOR
@@ -25,11 +25,28 @@ FastADC::FastADC(){
 	Old_ADCSRA = 0;
 	Old_ADMUX = 0;
 	OnFastADC = 0;
-	}
+}
 	
 //========INSTANCE METHODS
 
-void FastADC::StartADCSpeed(int pin=0) { //8 bit 38 KSamples for second
+void FastADC::StartADCMaxSpeed(int pin=0) { //7 bit 74 KSamples for second
+	if (pin > 5) {	//Only pin A0 to A5 can be readen
+		pin=0;
+	}
+	if (~OnFastADC) {  //Saves old values of ACSRB, ACSRA, ADMUX
+		char OnFastADC=1;
+		Old_ADCSRB = ADCSRB;
+		Old_ADCSRA = ADCSRA;
+		Old_ADMUX = ADMUX;
+	}
+	ADMUX = (1<<REFS0)|(pin);
+	ADCSRB = 0;
+	PRR &= ~(PRADC>>1); //ADC power reduction
+	ADCSRA = ((1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (1<<ADPS1); //free running mode, prescaler = 16
+	sei();
+}
+
+void FastADC::StartADCSpeed(int pin=0) { //8 bit 37 KSamples for second
 	if (pin > 5) {	//Only pin A0 to A5 can be readen
 		pin=0;
 	}
@@ -43,6 +60,7 @@ void FastADC::StartADCSpeed(int pin=0) { //8 bit 38 KSamples for second
 	ADCSRB = 0;
 	PRR &= ~(PRADC>>1); //ADC power reduction
 	ADCSRA = ((1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (1<<ADPS0)  |(1<<ADPS2)); //free running mode, prescaler = 32
+	sei();
 }
 
 void FastADC::StartADCRes(int pin=0) { //10 bit 9 KSamples for second
@@ -59,15 +77,16 @@ void FastADC::StartADCRes(int pin=0) { //10 bit 9 KSamples for second
 	ADCSRB = 0;
 	PRR &= ~(PRADC>>1); //ADC power reduction
 	ADCSRA = ((1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (1<<ADIE) | (1<<ADPS0) | (1<<ADPS1) |(1<<ADPS2)); //free running mode, prescaler = 128
+	sei();
 }
 
 int FastADC::Get() {
 	sei();
-	while(newData==0);		//wait until new data are available
-	cli();
+	while(OnFastADC==1);		//wait until new data are available
+//	cli();
 	int i = analogValue;
-	newData = 0;			//invalidate data for future readingsa
-	sei();
+	OnFastADC = 1;			//invalidate data for future readingsa
+//	sei();
 	return i;				//return them
 }
 
